62328eeae5cde134ca3df98262a70570fe7ec3ea
daniloui001 <daniel.louie@stonybrook.edu>
2023-11-07 21:45:40 -0500

    add

---
+++ weight_checker/example_code.py
@@ -0,0 +1,20 @@
+import requests 
+import pandas as pd 
+
+df = pd.DataFrame({
+    'weight': [100, 121, 131, 140],
+    'height': [184, 160, 150, 175]
+})
+
+df[:1]['weight']
+
+
+analysis = requests.get(
+    url = 'https://us-east1-hants-504-2023.cloudfunctions.net/bp-checker-hants',
+    params = ({
+        "weight": df[:1]['weight'],
+        "height": df[:1]['height']
+    })
+)
+
+analysis.text
